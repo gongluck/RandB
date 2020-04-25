37acdeec25b2f427ef0e61228ca74b9cd9ec24ec
gongluck <1039994845@qq.com>
2020-04-25 18:46:26 +0800

    打包源码

--- books/VC++深入详解/code/Chapter19/Dll2/Dll2.cpp
+++
@@ -1,9 +0,0 @@
-int _stdcall add(int a,int b)
-{
-	return a+b;
-}
-
-int _stdcall subtract(int a,int b)
-{
-	return a-b;
-}
--- books/VC++深入详解/code/Chapter19/Dll3/Dll3.cpp
+++
@@ -1,4 +0,0 @@
-_declspec(dllexport) int add(int a,int b)
-{
-	return a+b;
-}
